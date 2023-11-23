3f24ce05f29229bd805a95ba486e7a305be0ed19
arnautdj <sfrosales@utpl.edu.ec>
2023-11-23 17:22:27 -0500

    Problema 1

---
+++ problematicas/problema01/problema01/src/problema01/Problema01.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Problema01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        String cadenaReporte = "";
+        String nombreJugador;
+        String posicionCampo;
+        double estatura;
+        int edad;
+        boolean bandera = true;
+        String salir;
+        int sumaEdades = 0;
+        double sumaEstaturas = 0;
+        int contadorIteraciones = 0;
+        double promedioEdad;
+        double promedioEstatura;
+        String edadAcumulacion = "";
+        cadenaReporte = String.format("%s%s\n", cadenaReporte, 
+                "Listado de Jugadores");
+        
+        while(bandera){
+            System.out.println("Ingrese el nombre del Jugador: ");
+            nombreJugador = entrada.nextLine();
+            System.out.println("Ingrese la posición en el campo: ");
+            posicionCampo = entrada.nextLine();
+            System.out.println("Ingrese la edad del Jugador: ");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del Jugador: ");
+            estatura = entrada.nextDouble();
+
+            sumaEdades = sumaEdades + edad;
+            sumaEstaturas = sumaEstaturas + estatura;
+            
+            edadAcumulacion = edadAcumulacion + edad + "\n";
+
+            contadorIteraciones = contadorIteraciones + 1; 
+
+            cadenaReporte = String.format("%s%d. %s -%s-, edad %d, "
+                    + "%.2f\n",
+                    cadenaReporte,
+                    contadorIteraciones,
+                    nombreJugador,
+                    posicionCampo,
+                    edad,
+                    estatura);
+            
+            entrada.nextLine(); 
+            System.out.println("Sigite (s), si desea terminar");
+            salir = entrada.nextLine();
+            if(salir.equals("s")){
+                bandera = false;
+            }
+        }
+        
+        promedioEdad = (double)sumaEdades/contadorIteraciones;
+        promedioEstatura = sumaEstaturas/contadorIteraciones;
+
+        cadenaReporte = String.format("%sListado de Edades\n%sPromedio de edades: %.2f\n"
+                +"Promedio de estaturas: %.2f", 
+                cadenaReporte, edadAcumulacion,  promedioEdad, promedioEstatura);
+ 
+        System.out.printf("%s\n", cadenaReporte);
+       
+    }
+    
+}
